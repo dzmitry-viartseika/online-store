5c54b5481ea4fa1f183c4b8c82baa21626513c50
Dmitry Verteyko <verteyko1990@gmail.com>
2020-02-09 19:13:10 +0200

    1

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="v-main-wrapper">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "v-main-wrapper"
+    }
+</script>
+
+<style scoped lang="scss">
+    .v-main-wrapper {
+
+    }
+</style>
